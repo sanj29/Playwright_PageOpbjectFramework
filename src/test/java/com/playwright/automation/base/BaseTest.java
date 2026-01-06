de075c28cb596cb792eaf95d10c2e3770558e8d2
Sanjay Singh <sanjay.singh.aus84@gmail.com>
2026-01-06 11:50:45 +0530

    Update BaseTest.java

--- src/test/java/com/playwright/automation/base/BaseTest.java
+++ src/test/java/com/playwright/automation/base/BaseTest.java
@@ -2,6 +2,7 @@ package com.playwright.automation.base;
 
 import com.microsoft.playwright.Page;
 import com.playwright.automation.factory.PlaywrightFactory;
+import com.playwright.automation.factory.PlaywrightDriverManager;
 import com.playwright.automation.pages.AAFTHomePage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -11,24 +12,26 @@ import java.util.Properties;
 public class BaseTest {
 
     protected PlaywrightFactory pf;
-    protected Page page;
-    protected AAFTHomePage homePage;
     protected Properties prop;
+    protected AAFTHomePage homePage;
 
     @BeforeMethod
     public void setup() {
         pf = new PlaywrightFactory();
         prop = pf.initProp();
-        page = pf.initBrowser(prop);
+        pf.initBrowser(prop);
 
+        Page page = PlaywrightDriverManager.getPage();
         homePage = new AAFTHomePage(page);
-        homePage.acceptCookeies(); // safe & optional
+        homePage.acceptCookeies(); // safe
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
+        Page page = PlaywrightDriverManager.getPage();
         if (page != null) {
             page.context().browser().close();
+            PlaywrightDriverManager.unload();
         }
     }
 }
