5ffc157079f9189aa41447b97cd5244e02a45611
Sanjay Singh <sanjay.singh.aus84@gmail.com>
2026-01-06 11:08:22 +0530

    Update BaseTest.java

--- src/test/java/com/playwright/automation/base/BaseTest.java
+++ src/test/java/com/playwright/automation/base/BaseTest.java
@@ -3,35 +3,32 @@ package com.playwright.automation.base;
 import com.microsoft.playwright.Page;
 import com.playwright.automation.factory.PlaywrightFactory;
 import com.playwright.automation.pages.AAFTHomePage;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeMethod;
 
 import java.util.Properties;
 
 public class BaseTest {
 
-    PlaywrightFactory pf;
-    Page page;
-   protected AAFTHomePage homePage;
-   protected Properties prop;
+    protected PlaywrightFactory pf;
+    protected Page page;
+    protected AAFTHomePage homePage;
+    protected Properties prop;
 
-    @BeforeTest
-    public void setup(){
+    @BeforeMethod
+    public void setup() {
         pf = new PlaywrightFactory();
         prop = pf.initProp();
-        page =pf.initBrowser(prop);
+        page = pf.initBrowser(prop);
+
         homePage = new AAFTHomePage(page);
-        homePage.acceptCookeies();
+        homePage.acceptCookeies(); // safe & optional
     }
 
-    @AfterClass
-    public  void tearDown(){
-        page.context().browser().close();
-    }
-
-    @AfterMethod
-    public void resetToHome(){
-        page.navigate("https://aaft.com/");
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (page != null) {
+            page.context().browser().close();
+        }
     }
 }
